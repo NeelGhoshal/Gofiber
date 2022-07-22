e679094c4593911d2f7aef6b5be14091744df93f
Neel <neelghoshal02@gmail.com>
2022-07-22 19:45:00 +0530

    Added User Routes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fiber/database"
+	"fiber/routes"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,11 +11,26 @@ import (
 func welcome(c *fiber.Ctx) error {
 	return c.SendString("Welcome to this API")
 }
+
+func setupRoutes(app *fiber.App) {
+	//welcome endpoint
+	app.Get("/api", welcome)
+	//user endpoints
+	app.Post("/api/users", routes.CreateUser)
+
+	app.Get("/api/users", routes.GetUsers)
+
+	app.Get("/api/users/:id", routes.GetUser)
+
+	app.Put("/api/users/:id", routes.UpdateUser)
+
+	app.Delete("/api/users/:id", routes.DeleteUser)
+}
 func main() {
 	database.ConnectDb()
 	app := fiber.New()
 
-	app.Get("/api", welcome)
+	setupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 }
--- routes/user.go
+++ routes/user.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"errors"
 	"fiber/database"
 	"fiber/models"
 
@@ -28,4 +29,97 @@ func CreateUser(c *fiber.Ctx) error {
 	database.Database.Db.Create(&user)
 	responseUser := CreateResponseUser(user)
 	return c.Status(200).JSON(responseUser)
+
+}
+
+func GetUsers(c *fiber.Ctx) error {
+	users := []models.User{}
+
+	database.Database.Db.Find(&users)
+	responseUsers := []User{}
+	for _, user := range users {
+		responseUser := CreateResponseUser(user)
+		responseUsers = append(responseUsers, responseUser)
+	}
+
+	return c.Status(200).JSON(responseUsers)
+}
+
+func findUser(id int, user *models.User) error {
+	database.Database.Db.Find(&user, "id = ?", id) //the ? will be replaced by the paramater value of id
+	// and the reference value of the user passesd in the params will be replaced by the found value
+	if user.ID == 0 {
+		return errors.New("user dosen't exist")
+	}
+	return nil
+}
+
+func GetUser(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id") //checking request for a paramater(id)
+
+	var user models.User
+
+	if err != nil {
+		return c.Status(400).JSON("Please ensure that :id is an integer")
+	}
+
+	if err := findUser(id, &user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+	responseUser := CreateResponseUser(user)
+
+	return c.Status(200).JSON(responseUser)
+}
+
+func UpdateUser(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id") //checking request for a paramater(id)
+
+	var user models.User
+
+	if err != nil {
+		return c.Status(400).JSON("Please ensure that :id is an integer")
+	}
+
+	if err := findUser(id, &user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	type UpdateUser struct {
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+	}
+
+	var updateData UpdateUser
+
+	if err := c.BodyParser(&updateData); err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+
+	user.Firstname = updateData.FirstName
+	user.LastName = updateData.LastName
+
+	database.Database.Db.Save(&user)
+
+	responseUser := CreateResponseUser(user)
+	return c.Status(200).JSON(responseUser)
+}
+
+func DeleteUser(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+
+	var user models.User
+
+	if err != nil {
+		return c.Status(400).JSON("Please ensure that :id is an integer")
+	}
+
+	if err := findUser(id, &user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	if err := database.Database.Db.Delete(&user).Error; err != nil {
+		return c.Status(404).JSON(err.Error())
+	}
+
+	return c.Status(200).SendString("Successfully Deleted USer")
 }
